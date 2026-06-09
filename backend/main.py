c350d8e76d8c75fa9b41c589a47c1697e40cd378
samarth-14 <samarthdiwakar0@gmail.com>
2026-06-09 17:24:22 +0530

    connect frontend to backend - auth working

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
